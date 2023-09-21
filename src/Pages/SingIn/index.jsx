4958465ec94ac4b35da7fb057c924557498dc0c5
David951118 <dsmontes95@gmail.com>
2023-09-20 21:39:55 -0500

    Add singIn

--- src/Pages/SingIn/index.jsx
+++ src/Pages/SingIn/index.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React, { useState } from 'react'; // Importa useState
+import Layout from '../../components/Layout';
 
-export default function SingIn() {
+export default function SignIn() { // Cambia el nombre a SignIn en lugar de SingIn
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para el inicio de sesión
+    console.log('Correo electrónico:', formData.email);
+    console.log('Contraseña:', formData.password);
+  };
+
   return (
     <Layout>
-
+      <div className="max-w-md mx-auto mt-8">
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-2xl mb-4">Inicio de Sesión</h2>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Correo Electrónico
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Correo Electrónico"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Contraseña
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <a className="text-blue-500 hover:underline cursor-pointer" href="#">
+              ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Iniciar Sesión
+            </button>
+          </div>
+        </form>
+      </div>
     </Layout>
-  )
+  );
 }
-
